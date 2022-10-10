3d6c9dc1a3fa4defd8039ed2a8708c6be90a59ac
3ef1rus <10kmmrza10k@mail.ru>
2022-10-10 23:34:44 +0300

    create main class and child class + create two constructors

--- Program.cs
+++ Program.cs
@@ -1,5 +1,41 @@
 ﻿namespace lab2
 {
+
+    class Product
+    {
+        string name, date;
+        int art;
+        float price;
+
+        public Product(string name, int art, float price, string date)
+        {
+            this.name = name;
+            this.art = art;
+            this.price = price;
+            this.date = date;
+        }
+        public Product()
+        {
+            this.name = "";
+            this.art = 0;
+            this.price = 0;
+            this.date = "";
+        }
+       /* public string name{ //getter and setter
+            set{name=value;}
+            get{return(name);}
+        }*/
+        
+    }
+    class Ball : Product
+    {
+        string color;
+        public Ball(string color, string name, int art, float price, string date) : base(name, art, price, date)
+        {
+            this.color = color;
+        }
+    }
+
     class NewClass
     {
         static void Main()
@@ -8,4 +44,5 @@
 
         }
     }
+
 }
