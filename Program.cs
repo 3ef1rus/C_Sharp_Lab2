ef165dfa22186b4f534e97f578527922e0b5fd9e
3ef1rus <10kmmrza10k@mail.ru>
2022-10-10 23:08:27 +0300

    clear proj

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace lab2
+{
+    class NewClass
+    {
+        static void Main()
+        {
+
+
+        }
+    }
+}
