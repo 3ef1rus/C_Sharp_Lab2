eb0b4eb317879b467ec9b9c1ecfe4810433f4b10
3ef1rus <10kmmrza10k@mail.ru>
2022-10-10 23:52:21 +0300

    create method print and make it virtual + create new method

--- Program.cs
+++ Program.cs
@@ -6,6 +6,19 @@
         string name, date;
         int art;
         float price;
+        private bool _status;
+        bool send;
+        public bool Status
+        { //getter and setter
+            get
+            {
+                return (_status);
+            }
+            set
+            {
+                _status = value;
+            }
+        }
 
         public Product(string name, int art, float price, string date)
         {
@@ -21,11 +34,28 @@
             this.price = 0;
             this.date = "";
         }
-       /* public string name{ //getter and setter
-            set{name=value;}
-            get{return(name);}
-        }*/
-        
+        /* */
+        public virtual void Print()
+        {
+            Console.WriteLine("Название товара :" + name);
+            Console.WriteLine("Артикул товара :" + art);
+            Console.WriteLine("Цена товара :" + price);
+            Console.WriteLine("Дата создания товара :" + date);
+            Console.WriteLine("");
+        }
+        public void Sendproduct()
+        {
+            if (this.send == false)
+            {
+                this.send = true;
+                Console.WriteLine("Товар с артикулом :" + art + " Отправлент");
+            }
+            else
+            {
+                Console.WriteLine("Товар с артикулом :" + art + " Уже в пути");
+            }
+        }
+
     }
     class Ball : Product
     {
@@ -33,6 +63,11 @@
         public Ball(string color, string name, int art, float price, string date) : base(name, art, price, date)
         {
             this.color = color;
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Цвет товара :" + color);
+            base.Print();
         }
     }
 
